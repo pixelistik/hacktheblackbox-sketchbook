8a751d050faf6fdc4be2488e1aff6fd25eb2f8f7
Pixelistik <code@pixelistik.de>
2015-09-04 14:48:54 +0200

    Include average over buffered readings

--- hack_the_blackbox/ScaredBot/ScaredBot.ino
+++ hack_the_blackbox/ScaredBot/ScaredBot.ino
@@ -19,7 +19,13 @@ int outputValue = 0;
 int knownSensorMax = 0;    // Init with lowest possible analog sensor max
 int knownSensorMin = 1023; // Init with highest possible analog sensor min
 
+const int bufferLength = 80;
+int buffer[bufferLength];
+int bufferPosition = 0;
+
 void setup() {    
+  Serial.begin(9600);
+  
   // Initialise servo  
   servo.attach(servoPin);
 }
@@ -37,13 +43,40 @@ void loop()
   if (sensorValue < knownSensorMin) {
     knownSensorMin = sensorValue;
   }
+  
+  sensorValue = sensorHistoryAvg(sensorValue);
 
   // Dark = high resistance = high sensor value
   // Map sensor value to servo angle
-  outputValue = map(sensorValue, knownSensorMin, knownSensorMax, 0, 180); 
+  outputValue = map(sensorValue, knownSensorMin, knownSensorMax, 0, 45);
 
   servo.write(outputValue);
 
   delay (10);
 }
 
+int sensorHistoryAvg(int latestValue) {
+  Serial.print("Storing value ");
+  Serial.print(latestValue);
+  Serial.print(" at position ");
+  Serial.print(bufferPosition);
+  
+  buffer[bufferPosition] = latestValue;
+  
+  bufferPosition++;
+  if (bufferPosition >= bufferLength) bufferPosition = 0;
+
+  // Calculate the average from the last readings
+  long sum = 0;
+  
+  for (int i = 0; i < bufferLength; i++) {
+    sum += buffer[i];
+  }  
+  Serial.print(sum);
+  int average = sum / bufferLength;
+  
+  Serial.print(", average is ");
+  Serial.print(average);
+  Serial.println("");
+  return average;
+}

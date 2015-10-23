ac8737ea6d3f52e3120a438eee7f7c08ce160e52
Pixelistik <code@pixelistik.de>
2015-10-23 22:09:20 +0200

    Refactor to a more flexible array pattern

--- hack_the_blackbox/UsbHid/SwitchToKeys/SwitchToKeys.ino
+++ hack_the_blackbox/UsbHid/SwitchToKeys/SwitchToKeys.ino
@@ -5,40 +5,26 @@
  * firmware, see http://mitchtech.net/arduino-usb-hid-keyboard/
  */
 
-bool SERIAL_DEBUG_PRINT = false;
+bool SERIAL_DEBUG_PRINT = true;
 
 // Keyboard report buffer
 uint8_t buf[8] = { 0 };
 
-const int LOWEST_PIN = 2;
-const int HIGHEST_PIN = 12;
+int pins[] = {    2,    3,    4,    5,    6,    7,    8,    9,   10,   11,   12 };
+int keys[] = { 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0B, 0x0D, 0x12, 0x14, 0x17 };
+//                a     b     c     d     e     f     h     j     o     q     t
 
-int keyMap[HIGHEST_PIN + 1] = {
-  0,
-  0,
-  0x04, // 2 = a
-  0x05, // 3 = b
-  0x06, // 4 = c
-  0x07, // 5 = d
-  0x08, // 6 = e
-  0x09, // 7 = f
-  0x0B, // 8 = h
-  0x0D, // 9 = j
-  0x12, // 10 = o
-  0x14, // 11 = q
-  0x17 //  12 = t
-
-};
+const int pinCount = sizeof(pins) / sizeof(int);
 
 int pinState;
 
-int pinStatePrevious[HIGHEST_PIN + 1];
+int pinStatePrevious[pinCount];
 
 void setup() {
   Serial.begin(9600);
 
-  for(int i = LOWEST_PIN; i <= HIGHEST_PIN; i++) {
-	pinMode(i, INPUT_PULLUP);
+  for(int i = 0; i <= pinCount; i++) {
+	pinMode(pins[i], INPUT_PULLUP);
   }
 
   delay(200);
@@ -64,12 +50,11 @@ void releaseKey() {
 }
 
 void loop() {
-
-	for(int i = LOWEST_PIN; i <= HIGHEST_PIN; i++) {
-		pinState = digitalRead(i);
+	for(int i = 0; i <= pinCount; i++) {
+		pinState = digitalRead(pins[i]);
 
 		if (pinStatePrevious[i] == 1 && pinState == 0) {
-			pressKey(keyMap[i]);
+			pressKey(keys[i]);
 		}
 
 		if (pinStatePrevious[i] == 0 && pinState == 1) {
